fc6120e83d0a520088c8f78d0ca1c9c5c43a53aa
Robin Elzeard <43992365+rorp24@users.noreply.github.com>
2023-01-17 09:50:38 +0100

    better implementation

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const webpush = require('web-push');
+var cors = require('cors')
 const appconf = require('./config/app.config.json');
 
 
@@ -8,9 +9,18 @@ webpush.setVapidDetails('mailto:'+appconf.mail, appconf.public_key, appconf.priv
 
 const app = express();
 var subscription
-
 app.use(require('body-parser').json());
+app.use(cors({
+  origin:"'*'",
+}))
+app.use(function (err, req, res, next) {
+  console.log(err)
+  res.send(500, 'Something broke!');
+  next()
+});
 
+//Note for future self: as their is an issue with chrome and localhosted servers, if you want to use something else than the localhost:3000 port
+//eather find a way to solve the issue, or just host that small test server on something (with the front part secured, of course.)
 app.post('/subscribe', (req, res) => {
   subscription = req.body;
   res.status(201).json({});
@@ -33,15 +43,15 @@ app.post('/new-notification-to-send',(req,res)=>{
     icon:'http://mongoosejs.com/docs/images/mongoose5_62x30_transparent.png',
     body:req.body.content
   });
-  console.log("test 1")
   setTimeout(()=>{
-    console.log("test 2")
     webpush.sendNotification(subscription, payload).catch(error => {
       console.error(error.stack);
     });
-  },15000)
+  },10000)
 })
 
 app.use(require('express-static')('./'));
 
-app.listen(3000);
+app.listen(3000,()=>{
+  console.log('up and running on localhost:3000')
+});
